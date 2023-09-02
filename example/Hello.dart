eaf52c838754a625bf8ed65630d2cbf9eb8ff082
faiszuddin <faeznasir19@gamil.com>
2023-09-02 23:46:57 +0800

    dart library

---
+++ example/Hello.dart
@@ -0,0 +1,17 @@
+import "package:belajar_dart_library/Hello.dart";
+
+void main() {
+
+  print(sayHello("Fais"));
+
+  var cust = Customer();
+  var product = Product();
+  var category = Category()
+
+
+}
+
+
+
+
+
---
+++ lib/Hello.dart
@@ -0,0 +1,5 @@
+library belajar_dart_library;
+
+export 'src/sayHello.dart';       //cara 1: expost all in class sayHello
+// export 'src/sayHEllo.dart' show sayHai;  //untuk specific apa sahaja yang perlu diexpost
+export 'src/Customer.dart';
